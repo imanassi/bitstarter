05e486bd18563c282abc00b4f44de6f8ab1444f8
imanassi <imanassi@gmail.com>
2013-07-10 12:59:55 +0300

    reading from index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var result = fs.readFileSync('index.html');
+	console.log(result.toString());
+	response.send(result.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
